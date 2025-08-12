9a4526035ab56de3e92cebef75792a2a176577b4
minjeong46 <eyou46@naver.com>
2025-08-13 00:37:22 +0900

    feat. useMemo로 최적화해보기

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
@@ -21,9 +21,24 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
     const filteredTodos = getFilteredData();
 
+    // state 상태에 따라 계속 호출되던 부분을 useMemo로 해결, useMemo로 하지 않으면 검색어에 입력할 때마다 호출됨
+    const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+        console.log("호출 확인!");
+        const totalCount = todos.length;
+        const doneCount = todos.filter((todo) => todo.isDone).length;
+        const notDoneCount = totalCount - doneCount;
+
+        return { totalCount, doneCount, notDoneCount };
+    }, [todos]);
+
     return (
         <div className="List">
             <h4>Todo List ✅</h4>
+            <div className="todos_count">
+                <div>전체 Todo: {totalCount}</div>
+                <div>완료한 Todo: {doneCount}</div>
+                <div>미완료한 Todo: {notDoneCount}</div>
+            </div>
             <input
                 value={search}
                 onChange={onChangeSearch}
@@ -31,7 +46,14 @@ const List = ({ todos, onUpdate, onDelete }) => {
             ></input>
             <div className="todos_wrapper">
                 {filteredTodos.map((todo) => {
-                    return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete}  />;
+                    return (
+                        <TodoItem
+                            key={todo.id}
+                            {...todo}
+                            onUpdate={onUpdate}
+                            onDelete={onDelete}
+                        />
+                    );
                 })}
             </div>
         </div>
